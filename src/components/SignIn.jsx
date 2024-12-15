849189d320d52199e163c7f323a826a615367b25
ThisIsLuckyluck <denislucas1a@gmail.com>
2024-12-15 18:53:11 +0100

    adding login function to communicate with actual backend

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,13 +1,50 @@
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Logo from '../../assets/icons/logo_blanc.png';
 import styles from '../styles/App.module.css';
 import '../styles/global.css';
 
 export default function SignIn() {
+  const [LogEmail, setLogEmail] = useState('');
+  const [LogPassword, setLogPassword] = useState('');
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    navigate('/home');
+  useEffect(() => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      navigate('/home');
+    }
+  }, [navigate]);
+
+  const login = () => {
+    const formData = new URLSearchParams();
+    formData.append('email', LogEmail);
+    formData.append('password', LogPassword);
+
+    fetch('http://localhost:3000/users/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: formData.toString(),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Erreur lors de la connexion');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // console.log(data.user.token);
+        if (data.user.token) {
+          localStorage.setItem('authToken', data.user.token);
+          return navigate('/home');
+        }
+        throw new Error('Token non trouvé dans la réponse');
+      })
+      .catch((error) => {
+        console.error('Erreur:', error);
+      });
   };
 
   return (
@@ -24,20 +61,20 @@ export default function SignIn() {
           <p className={styles.CredentialsText}>Your username :</p>
           <input
             type="text"
-            placeholder="username"
+            placeholder="name"
             className={styles.CredentialsField}
+            onChange={(e) => setLogEmail(e.target.value)}
+            value={LogEmail}
           />
           <p className={styles.CredentialsText}>Your password :</p>
           <input
             type="password"
             placeholder="password"
             className={styles.CredentialsField}
+            onChange={(e) => setLogPassword(e.target.value)}
+            value={LogPassword}
           />
-          <button
-            className={styles.LoginButton}
-            type="button"
-            onClick={handleLogin}
-          >
+          <button className={styles.LoginButton} type="button" onClick={login}>
             Login
           </button>
         </div>
